3b7b21950c5a84a48e1a74d038161c144591d77e
root <root@localhost.localdomain>
2026-08-06 20:34:02 +0530

    Fix main.dart merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'screens/home_screen.dart';
-
-void main() {
-  runApp(const UltraCutApp());
-}
-
-class UltraCutApp extends StatelessWidget {
-  const UltraCutApp({super.key});
-=======
 import 'theme/app_theme.dart';
 import 'screens/home/home_screen.dart';
 
@@ -18,24 +8,14 @@ void main() {
 
 class NovaCutUltraAI extends StatelessWidget {
   const NovaCutUltraAI({super.key});
->>>>>>> 1989033bc11077ab029cb021f09ee2ede67a5ed3
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      title: 'UltraCut',
-      theme: ThemeData.dark(),
-=======
       title: 'NovaCut Ultra AI',
       theme: AppTheme.darkTheme,
->>>>>>> 1989033bc11077ab029cb021f09ee2ede67a5ed3
       home: const HomeScreen(),
     );
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 1989033bc11077ab029cb021f09ee2ede67a5ed3
